e21a0605893db0aa134d8b17e8161d88feaa6d47
RomanVitolo <68485569+RomanVitolo@users.noreply.github.com>
2021-08-10 03:26:54 -0300

    Add LineOfSight + Obstacle Detection

---
+++ Artificial Intelligence - Final Previo - Vitolo Roman/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public bool inSight;
+    private LineOfSight _lineOfSight;
+    private Rigidbody rb;
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        _lineOfSight = GetComponent<LineOfSight>();
+    }
+
+     void Update()
+    {
+        if (_lineOfSight.IsInSight(target))
+        {
+            Debug.Log("Veo al Enemigo");
+            inSight = true;
+        }
+        else
+        {
+            Debug.Log("No veo al enemigo");
+            inSight = false; 
+        }
+        
+        
+    }
+}
---
+++ Artificial Intelligence - Final Previo - Vitolo Roman/Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    [SerializeField] private float range;
+    [SerializeField] private float angle;
+    [SerializeField] private LayerMask mask;
+
+    public bool IsInSight(Transform target)
+    {        
+        Vector3 diff = (target.position - transform.position);
+        float distance = diff.magnitude;
+        if (distance > range) return false;
+        float angleToTarget = Vector3.Angle(transform.forward, diff.normalized);
+        if (angleToTarget > angle / 2) return false;
+        if (Physics.Raycast(transform.position, diff.normalized, distance, mask))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * range);
+        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, angle /2, 0) * transform.forward * range);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, -angle / 2, 0) * transform.forward * range);
+    }
+}
